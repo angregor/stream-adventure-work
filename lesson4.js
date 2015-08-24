abcb94e8af3671651b3459d99bfec39a606478fc
Andrew Gregory <angregor@asu.edu>
2015-08-23 19:48:59 -0700

    lesson4 not complete, still have errors

--- lesson4.js
+++ lesson4.js
@@ -1,1 +1,7 @@
-//in progress
+var through = require('through2');
+var information = '';
+
+var stream = through(function(buffer) {
+  this.queue(buffer.toString().toUpperCase());
+});
+process.stdin.pipe(stream).pipe(process.stdout);
